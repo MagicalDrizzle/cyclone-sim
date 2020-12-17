46198670092d91f72e4b46662118a740a876b7c4
Mizuki <49761211+MagicalDrizzle@users.noreply.github.com>
2020-12-17 14:10:03 +0000

    Update ui.js

--- ui.js
+++ ui.js
@@ -424,7 +424,7 @@ UI.init = function(){
         textSize(18);
         textStyle(NORMAL);
         text("I don't know what I'm doing I pretty much just took PB's fork and screw around with numbers till I'm bored...",0,40); 
-        text("I suck at code and computers in general :<",0,75);
+        text("I suck at code and computers in general :<",0,65);
     });
 
     mainMenu.append(false,WIDTH/2-100,HEIGHT/2-20,200,40,function(s){    // "New Basin" button
